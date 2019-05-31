70199e18c19957d8ab5869ad9553d7eeb8cc69eb
litaodior <diorlitao@gmail.com>
2019-05-31 14:26:30 +0800

    测试写入文件中包含转义字符

--- node3/updatefile.js
+++ node3/updatefile.js
@@ -1,8 +1,9 @@
 const fs = require('fs')
-fs.appendFile('./writefile.txt', 'nihao zhongguo', (err) => {
+fs.appendFile('./writefile.txt', '\n nihao zhongguo', (err) => {
   if (err) {
     console.log('failure')
   } else {
     console.log('ok')
   }
 })
+// nodejs中写人的内容可以是转义字符，
