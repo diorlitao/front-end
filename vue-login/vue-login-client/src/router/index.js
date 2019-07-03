66514720e0ee8295c2f72b8e66342ac7ed37b884
litaodior <diorlitao@gmail.com>
2019-07-03 13:57:41 +0800

    vue-login 代码初始化

---
+++ vue-login/vue-login-client/src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// 懒加载路由
+const login = () => import('@/components/Login')
+const index = () => import('@/components/Index')
+Vue.use(Router)
+
+export default new Router({
+  routes: [{
+    path: '/login',
+    name: 'login',
+    component: login
+  }, {
+    path: '/index',
+    name: 'index',
+    component: index
+  },
+  {
+    path: '*',
+    redirect: '/login'
+  }
+  ]
+})
